f0d92751a441519bd6966594835ff5dc9b700fa8
Szajko Peter <szajko.peter@bolyai.local>
2026-02-06 07:55:48 +0100

    ropi

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const input1 = document.createElement('input')
+body.appendChild(input1)
+
+const input2 = document.createElement('input')
+body.appendChild(input2)
+
+const result = document.createElement('div')
+body.appendChild(result)
+
+
+
